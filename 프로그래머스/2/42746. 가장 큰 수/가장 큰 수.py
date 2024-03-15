33b17e2b79f73a88e6bfa8c31021530b93a2adf3
김용찬 <96067127+yongchann@users.noreply.github.com>
2024-03-15 19:06:03 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,5 +1,12 @@
+from functools import cmp_to_key
+
 def solution(numbers):
     numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x*4, reverse=True)
+    
+    # cmp_to_key
+    #    positive return means switching
+    #    negative return means non-switching
+    numbers.sort(key=cmp_to_key(lambda x, y : -1 if int(x+y) > int(y+x) else 1))
+    
     return str(int("".join(numbers)))
 
