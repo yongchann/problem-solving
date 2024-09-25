8356717d64eb2bb43c3adf6503f4e64da12e1bd5
김용찬 <yongcgp@naver.com>
2024-09-25 18:07:14 +0900

    [level 2] Title: 오픈채팅방, Time: 144.65 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,21 +1,11 @@
-from collections import defaultdict
-
 def solution(records):
     answer = []
-    uids = defaultdict(str)
+    uids = {record.split()[1]: record.split()[2] for record in records if record.split()[0] != 'Leave'}
     
     for record in records:
-        cmd = record.split()
-        if cmd[0] == 'Enter':
-            uids[cmd[1]] = cmd[2]
-            answer.append(cmd[1]+"님이 들어왔습니다.")
-        elif cmd[0] == 'Leave':
-            answer.append(cmd[1]+"님이 나갔습니다.")
-        elif cmd[0] == 'Change':
-            uids[cmd[1]] = cmd[2]
-            
-    for i in range(len(answer)):
-        uid = answer[i].split("님이")[0]
-        answer[i] = answer[i].replace(uid, uids[uid])
-        
+        log = record.split()
+        if log[0] == "Enter":
+            answer.append(uids[log[1]]+'님이 들어왔습니다.')     
+        elif log[0] == 'Leave':
+            answer.append(uids[log[1]]+'님이 나갔습니다.')     
     return answer
