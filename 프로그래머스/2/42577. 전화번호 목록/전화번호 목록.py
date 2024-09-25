c83993e852acfda582232fc13c69803ed45e9e00
김용찬 <yongcgp@naver.com>
2024-09-25 18:12:29 +0900

    [level 2] Title: 전화번호 목록, Time: 95.48 ms, Memory: 30.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,6 +1,6 @@
-def solution(phone_book):
-    phone_book.sort()
-    for i in range(len(phone_book)-1):
-        if phone_book[i+1].startswith(phone_book[i]):
+def solution(data):
+    data.sort()
+    for i in range(len(data)-1):
+        if data[i+1].startswith(data[i]):
             return False
     return True
