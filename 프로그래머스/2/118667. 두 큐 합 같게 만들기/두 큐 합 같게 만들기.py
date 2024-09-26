58c02526d9c632c0944679214cc01beec3ba1ab6
김용찬 <yongcgp@naver.com>
2024-09-26 21:17:39 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 66.44 ms, Memory: 40 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,32 +1,25 @@
+def solution(queue1, queue2):
+    q = queue1 + queue2
+    target, remain = divmod(sum(q), 2)
+    if remain != 0:
+        return -1
 
-def solution(q1, q2):
-    q = q1 + q2
-    target = sum(q)//2
-    answer = 0
-    
-    # 누적합 저장
-    prefix_sum = [0] * (len(q)+1)
-    for i in range(1, len(q)+1):
-        prefix_sum[i] = prefix_sum[i-1] + q[i-1]
-    
-    # 두개의 포인터
-    p1, p2 = 0, len(q)//2
-    
+    idx1, idx2, last = 0, len(queue1), len(q)
+    curr = sum(q[:idx2])
+    answer = 0   
     while True:
-        if p1 >= p2 or p2 > len(q):
-            answer = -1
-            break
-            
-        diff = prefix_sum[p2] - prefix_sum[p1]
+        if idx1 >= idx2 or idx2 == last:
+            return -1
         
-        if diff == target:
-            break
-        elif diff > target:
-            p1 += 1
-            answer += 1
-        elif diff < target:
-            p2 += 1
+        if curr == target:
+            return answer
+        
+        if curr < target:
+            curr += q[idx2]
+            idx2 += 1
             answer += 1
             
-    return answer
-    
+        elif curr > target:
+            curr -= q[idx1]
+            idx1 += 1
+            answer += 1
