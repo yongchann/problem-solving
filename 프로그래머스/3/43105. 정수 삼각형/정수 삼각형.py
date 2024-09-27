f89893875da14ad418a8fcd88a689ea477781b3f
김용찬 <yongcgp@naver.com>
2024-09-27 12:27:26 +0900

    [level 3] Title: 정수 삼각형, Time: 46.23 ms, Memory: 19.1 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,11 +1,16 @@
 def solution(triangle):
-    # 위 두개 중 큰 값을 누적
-    for i in range(1, len(triangle)):
+    level = len(triangle)
+    dp = [[0] * i for i in range(1, level+1)]
+    dp[0][0] = triangle[0][0]
+    
+    for i in range(1, level):
         for j in range(i+1):
             if j == 0:
-                triangle[i][j] += triangle[i-1][j]
+                dp[i][0] = dp[i-1][0] + triangle[i][0]
             elif j == i:
-                triangle[i][j] += triangle[i-1][j-1]
+                dp[i][-1] = dp[i-1][-1] + triangle[i][-1]
             else:
-                triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
-    return max(triangle[-1])
+                dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
+            
+    return max(dp[-1])
+    
