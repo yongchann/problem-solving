8b2202d513618715bda8a8baf83711a9974e6e81
김용찬 <yongcgp@naver.com>
2024-09-27 12:30:19 +0900

    [level 3] Title: 정수 삼각형, Time: 45.79 ms, Memory: 19.1 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,9 +1,7 @@
 def solution(triangle):
     level = len(triangle)
     dp = [[0] * i for i in range(1, level+1)]
-    dp[0][0] = triangle[0][0]
-    
-    for i in range(1, level):
+    for i in range(level):
         for j in range(i+1):
             if j == 0:
                 dp[i][0] = dp[i-1][0] + triangle[i][0]
