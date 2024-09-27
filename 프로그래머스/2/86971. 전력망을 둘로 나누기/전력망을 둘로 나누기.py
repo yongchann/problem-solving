378da33ce7e2443a23a94d5cdbbed9b6cd51ccd4
김용찬 <yongcgp@naver.com>
2024-09-27 13:09:57 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.93 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,25 +1,19 @@
 from collections import deque
 
 def bfs(n, graph):
-    areas = []
+    start, area_size = 1, 1
     visited = [False] * (n+1)
-    for node in range(1, n+1):
-        if not visited[node]: # 첫 방문 노드에 대해 bfs 탐색
-            area_size = 1
-            visited[node] = True
-            q = deque([node])
-            
-            while q:
-                now = q.popleft()
-                for next_node in graph[now]:
-                    if not visited[next_node]:
-                        visited[next_node] = True
-                        area_size += 1
-                        q.append(next_node)
-                    
-            areas.append(area_size)
-            
-    return areas
+    visited[start] = True
+    q = deque([start])
+    
+    while q:
+        node = q.popleft()
+        for next_node in graph[node]:
+            if not visited[next_node]:
+                visited[next_node] = True
+                area_size += 1
+                q.append(next_node)
+    return area_size
 
 def solution(n, wires):
     min_diff = float('inf')
@@ -35,12 +29,11 @@ def solution(n, wires):
         graph[wire[1]].remove(wire[0])
         
         # 그래프 탐색
-        areas = bfs(n, graph)
-        min_diff = min(min_diff, abs(areas[0]-areas[1]))
+        count = bfs(n, graph)
+        min_diff = min(min_diff, abs(count - (n-count)))
         
         # 연결 복원
         graph[wire[0]].add(wire[1])
         graph[wire[1]].add(wire[0])
         
     return min_diff
-        
