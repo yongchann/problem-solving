4917d863b50dc8e8207701c4e60334e59bfaa175
김용찬 <yongcgp@naver.com>
2025-02-28 02:16:09 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,11 +1,11 @@
 def solution(n):
-    bin_n = bin(n)[2:].zfill(20)
-
-    # 오른쪽부터 탐색하여 처음 등장한 '01' -> '10' 으로 수정
-    pos = bin_n.rfind('01')
-    lbin = list(bin_n)
-    lbin[pos], lbin[pos+1] = '1', '0'
+    def get_cnt(k):
+        return str(bin(k))[2:].count('1')
     
-    next_bin = ''.join(lbin[:pos+2] + sorted(lbin[pos+2:]))
-    return int(next_bin, 2)
-
+    cnt = get_cnt(n)
+    target = n + 1
+    while True:
+        if get_cnt(target) == cnt:
+            return target
+        target += 1
+        
